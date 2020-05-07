36da6bd6a2dc678a09ba0b7b912c8e6d599046fd
Fernando Bellas Permuy <fbellas@udc.es>
2020-05-07 10:31:29 +0200

    Se permite el uso del  plugin Redux DevTools.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import App from './App';
 import reducer from './reducer';
 
 /* Configure store. */
-const store = createStore(reducer);
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 /* Render application. */
 ReactDOM.render(
