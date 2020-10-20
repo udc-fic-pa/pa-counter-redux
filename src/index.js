e77ad28f0b06723925f3bd6077628f536f52e277
Fernando Bellas Permuy <fbellas@udc.es>
2020-10-20 18:28:37 +0200

    Actualización al código generado por la versión actual de Create React App.

--- src/index.js
+++ src/index.js
@@ -13,9 +13,11 @@ const store = createStore(reducer,
 
 /* Render application. */
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>, 
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
     document.getElementById('root'));
 
 serviceWorker.register();
